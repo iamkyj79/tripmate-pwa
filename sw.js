c1a31ac617ba5279d3157a661c86431c24b2362f
iamkyj79 <iamkyj79@gmail.com>
2026-09-10 20:36:35 +0900

    Add service worker for installable PWA

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='tripmate-shell-v53';
+const SHELL=['/','/index.html','/manifest.webmanifest','/tripmate-icon.svg','/pwa.js'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/index.html'))))});
